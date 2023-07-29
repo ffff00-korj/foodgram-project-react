1659bd902640b35775bb6fa0818b6fbe36e1181c
iborovkov <iborovkov97@yandex.ru>
2023-07-29 11:47:00 +0300

    modify recipe ser

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -73,7 +73,6 @@ class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = ('id', 'name', 'measurement_unit')
-        read_only_fields = ('id', 'name', 'measurement_unit')
 
 
 class RecipeSerializer(serializers.ModelSerializer):
