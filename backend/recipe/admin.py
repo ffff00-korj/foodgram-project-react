d4921a7c689d731379dece31b528ab652d21f535
iborovkov <ilia97borovkov@gmail.com>
2023-08-05 13:52:02 +0300

    recipe admin fix

--- backend/recipe/admin.py
+++ backend/recipe/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
+from django.utils.safestring import mark_safe
 
 from recipe.models import (
     FavoriteRecipe,
@@ -16,6 +17,7 @@ from recipe.models import (
 class RecipeIngrideintInline(admin.TabularInline):
     model = RecipeIngrideint
     extra = 1
+    min_num = 1
 
 
 class IngredientResource(resources.ModelResource):
@@ -42,20 +44,31 @@ class RecipeAdmin(admin.ModelAdmin):
         'name',
         'cooking_time',
         'author',
+        '_image',
         '_ingridients',
         '_tags',
+        '_favorite_count',
     )
     list_editable = ('author', 'name')
     search_fields = ('author', 'name', '_tags')
     inlines = (RecipeIngrideintInline,)
 
     @admin.display(description='ингридиенты')
-    def _ingridients(self, obj):
-        return [ingredient.name for ingredient in obj.ingredients.all()]
+    def _ingridients(self, recipe):
+        return [ingredient.name for ingredient in recipe.ingredients.all()]
 
     @admin.display(description='теги')
-    def _tags(self, obj):
-        return [tag.name for tag in obj.tags.all()]
+    def _tags(self, recipe):
+        return [tag.name for tag in recipe.tags.all()]
+
+    @admin.display(description='кол-во добавлений в избранное')
+    def _favorite_count(self, recipe):
+        return recipe.favorite.count()
+
+    @admin.display(description='картинка')
+    def _image(self, recipe):
+        return mark_safe(f'<img src={recipe.image.url} '
+                         'width="80" height="60">')
 
 
 @admin.register(ShoppingList)
